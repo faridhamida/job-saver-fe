7e11da4d2eb0f9dfd9eb1841a05f41377ca8d68c
Farid Hamida <faridhamida22@gmail.com>
2020-07-24 10:33:25 -0700

    Working on header

--- job-saver/src/routes/HomeHeader.tsx
+++ job-saver/src/routes/HomeHeader.tsx
@@ -3,7 +3,19 @@ import { OriginalBackground, Logo, StyledButton } from "../utils/styles.js";
 import { Button, Container, Header } from "semantic-ui-react";
 import logo from "../images/job-saver-logo.png";
 import movie from "../images/movie.mp4";
-// import Styled from "styled-components";
+import Styled from "styled-components";
+
+const OverLay = Styled.div`
+position: fixed;
+top: 0;
+left: 0;
+width:100%;
+height:100%;
+z-index: -1;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
 
 
 function HomeHeader() {
@@ -11,9 +23,10 @@ function HomeHeader() {
         <div>
             <video width="100%" height="100%" autoPlay loop muted>
                 <source src={movie} type="video/mp4"/>
-                    <Logo src={logo} alt="logo"/>
-                    <Header as="h2" content="Keep your job search organized and all in one place"/>
+                   
             </video>
+            <Logo src={logo} alt="logo"/>
+                    <Header as="h2" content="Keep your job search organized and all in one place"/>
             <OriginalBackground>
                 <Container textAlign="center">
                         
