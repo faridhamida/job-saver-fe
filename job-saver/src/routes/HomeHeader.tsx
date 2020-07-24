fba06d2c69e079c49f0ec55530dd48af316947b4
Farid Hamida <faridhamida22@gmail.com>
2020-07-23 17:54:53 -0700

    Adding video background

--- job-saver/src/routes/HomeHeader.tsx
+++ job-saver/src/routes/HomeHeader.tsx
@@ -2,24 +2,30 @@ import React from "react";
 import { OriginalBackground, Logo, StyledButton } from "../utils/styles.js";
 import { Button, Container, Header } from "semantic-ui-react";
 import logo from "../images/job-saver-logo.png";
+import movie from "../images/movie.mp4";
+// import Styled from "styled-components";
+
 
 function HomeHeader() {
     return (
         <div>
-            <OriginalBackground>
-                <Container text textAlign="center">
+            <video width="100%" height="100%" autoPlay loop muted>
+                <source src={movie} type="video/mp4"/>
                     <Logo src={logo} alt="logo"/>
                     <Header as="h2" content="Keep your job search organized and all in one place"/>
-                
-                <StyledButton
-                    animated="fade"
-                    size="huge"
-                    href="#"
-                >
+            </video>
+            <OriginalBackground>
+                <Container textAlign="center">
+                        
+                        <StyledButton
+                            animated="fade"
+                            size="huge"
+                            href="#"
+                        >
 
-                    <Button.Content visible>Get Started</Button.Content>
-                    <Button.Content hidden>Install Extension</Button.Content>
-                </StyledButton>
+                        <Button.Content visible>Get Started</Button.Content>
+                        <Button.Content hidden>Install Extension</Button.Content>
+                    </StyledButton>
             </Container>
         </OriginalBackground>
     </div>
