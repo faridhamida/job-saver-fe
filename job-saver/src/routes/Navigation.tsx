031e6133c91641a1e2e61755a404d9d0b13770b7
Kavin Tjhan <ktjhan@Kavins-MBP.hsd1.ca.comcast.net>
2020-07-29 10:24:02 -0700

    navigation logo added

--- job-saver/src/routes/Navigation.tsx
+++ job-saver/src/routes/Navigation.tsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, Image } from "semantic-ui-react";
+import logo-icon from "../images/logo-icon.png";
 
-function Navigation() {
+
+function Navigation(){
     const [activeItem, setActiveItem] = useState();
   
     return (
         <Menu className="nav">
+            <Menu.Item
+                as={Link}
+                to="/"
+                name="job-saver"
+                // active={activeItem === "Save this Job"}
+                // onClick={() => handleItemClick("save_this_job")}
+                >
+                {<Image src={logo-icon} size="tiny" spaced alt="save this job" />}
+            </Menu.Item>
             <Menu.Item
                 as={Link}
                 to="/login"
